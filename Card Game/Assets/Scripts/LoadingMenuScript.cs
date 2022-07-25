d53137f09d9abf054d74e2a5685b89b7b6c5c386
ShkoliarenkoTech <shkolyarenko.zp106@gmail.com>
2022-07-25 20:05:36 +0200

    .
    .

--- Card Game/Assets/Scripts/LoadingMenuScript.cs
+++ Card Game/Assets/Scripts/LoadingMenuScript.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class LoadingMenuScript : MonoBehaviour
 {
+
     public void Loading()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Loading();
+        }
+
+    }
 }
