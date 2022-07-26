173d9ed0f431f7299e848adfcbebae25a1040152
AnitaBarna <barna.anita98@gmail.com>
2022-07-26 15:17:55 +0200

    added ScriptableObjects

--- Card Game/Assets/Scripts/CardData.cs
+++ Card Game/Assets/Scripts/CardData.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Card", menuName = "CardGame/Card")]
 public class CardData : ScriptableObject
 {
+    [Header("Text Boxes")]
     public string cardTitle;
     public string description;
+    public string specialEffect;
 
+    [Header("Effect values")]
     public int damage;         // damage to opponent health
-    public int blackStrength;  // damage to Dr. Schmuck's strength
+    public int blackStrength;  // damage/boost to Dr. Schmuck's strength
     public int strength;       // cost/boost to strength of player 
     public int health;         // cost/boost to health of player
 
+    [Header("Special Effect values")]
     public int maxStrenght;    // change to maxStrenght
     public int maxHealth;      // change to maxHealth
 
+    [Header("Card Sprites")]
     public Sprite cardFront;
     public Sprite cardBack;
     public Sprite cardImage;
-    public Sprite effect1Sprite;
-    public Sprite effect2Sprite;
-    public Sprite effect3Sprite;
-    public Sprite effect4Sprite;
 
+    [Header("")]
     public int numberInDeck;
 
+    [Header("Ticking Plushie int value")]
+    public int numberOfTurnsSkippedAfter; // for Ticking Plushie card
 
 }
